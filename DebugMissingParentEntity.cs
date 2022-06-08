c951e1c189d77d793bdb925d41f7e2338f5b5cc8
WhiteThunder <whetethunger@gmail.com>
2022-06-07 23:29:10 -0700

    Initial commit

---
+++ DebugMissingParentEntity.cs
@@ -0,0 +1,57 @@
+using Oxide.Core.Libraries.Covalence;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Oxide.Plugins
+{
+    [Info("Debug Missing Parent Entity", "WhiteThumder", "1.0.0")]
+    [Description("Identifies and optionally kills entities that are missing their parent.")]
+    public class DebugMissingParentEntity : CovalencePlugin
+    {
+        [Command("debugMissingParents")]
+        private void Command(IPlayer player, string cmd, string[] args)
+        {
+            if (!player.IsServer)
+                return;
+
+            var foundObjects = new List<BaseNetworkable>();
+            var reportLines = new List<string>();
+
+            foreach (var networkable in BaseNetworkable.serverEntities)
+            {
+                if (!networkable.parentEntity.IsSet())
+                    continue;
+
+                var parentEntityId = networkable.parentEntity.uid;
+                if (parentEntityId == 0)
+                    continue;
+
+                //var parentEntityId = networkable.parentEntity.uid;
+                var parent = networkable.GetParentEntity();
+                if (parent == null && networkable != null && !networkable.IsDestroyed)
+                {
+                    foundObjects.Add(networkable);
+                    reportLines.Add($"{networkable.net.ID} (parent: {parentEntityId}) | {networkable.GetType()} | {networkable.PrefabName} @ {networkable.transform.position}");
+                }
+            }
+
+            if (reportLines.Count == 0)
+            {
+                player.Reply($"No entities found that are missing their parent. Hooray!");
+                return;
+            }
+
+            var reportString = string.Join("\n", reportLines);
+            player.Reply($"Entities that are missing their parent:\n{reportString}");
+
+            if (args.FirstOrDefault()?.ToLower() == "kill")
+            {
+                foreach (var networkable in foundObjects)
+                {
+                    networkable.Kill();
+                }
+                player.Reply($"Killed {foundObjects.Count} entities that were missing their parent.");
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,44 @@
+## What problem does this solve?
+
+Ever seen an error like this spammed in console?
+
+```
+UpdateNetworkGroup: Missing parent entity 7123456
+```
+
+This typically happens after a server restart if an entity was configured to save across reboots, while its parent entity was not, causing the child entity to keep looking for the parent every 2 seconds, continuously spamming the server console.
+
+Note: Not all entities that are missing their parent will spam this error, so this plugin may reveal additional entities which can probably be killed.
+
+## What should I do?
+
+**Diagnose** -- Run the `debugMissingParents` command in the **server console** to see which entities are missing their parent. This information will give you a directional indicator of which circumstances are causing the issue to arise in the first place. For example, plugins might be spawning entities and parenting them to entities that are not being saved, therefore causing this issue. It could also be a vanilla Rust bug.
+
+**Mitigate** -- Run the `debugMissingParents kill` command to kill all entities that are missing their parent.
+
+## Commands
+
+- `debugMissingParents` -- Prints out information about all entities that are missing their parent.
+  Example output:
+- `debugMissingParents kill` -- Kills all entities that are missing their parent.
+
+Example output:
+
+```
+> debugMissingParents
+Entities that are missing their parent:
+1360496 (parent: 1360485) | BaseChair | assets/bundled/prefabs/static/chair.static.prefab @ (0.2, 0.0, -1.4)
+1360518 (parent: 1360497) | BaseChair | assets/bundled/prefabs/static/chair.static.prefab @ (0.2, 0.0, -1.4)
+1360678 (parent: 1360667) | BaseChair | assets/bundled/prefabs/static/chair.static.prefab @ (0.2, 0.0, -1.4)
+1360769 (parent: 1360748) | BaseChair | assets/bundled/prefabs/static/chair.static.prefab @ (0.2, 0.0, -1.4)
+```
+
+```
+> debugMissingParents kill
+Killed 4 entities that were missing their parent.
+```
+
+```
+> debugMissingParents
+No entities found that are missing their parent. Hooray!
+```
